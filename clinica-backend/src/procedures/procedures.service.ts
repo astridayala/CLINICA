023abb31a2a97a0321a243429e70cd1a86acf584
astridayala <astriayala06@gmail.com>
2025-08-22 22:48:23 -0600

    endpoint procedures

--- clinica-backend/src/procedures/procedures.service.ts
+++ clinica-backend/src/procedures/procedures.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Procedure } from './procedure.entity';
 import { Repository } from 'typeorm';
+import { CreateProcedureDto } from './dto/create-procedures.dto';
 
 /**
  * Servicio para gestionar los procedimientos
@@ -15,6 +16,47 @@ export class ProceduresService {
     ) {}
 
     /**
-     * 
+     * Crea un nuevo procedimiento
+     * @param createProceduresDto - DTO del procedimiento
+     * @returns El procedimiento creado
      */
+    async create(createProceduresDto: CreateProcedureDto): Promise<Procedure> {
+        const newProcedure = this.procedureRepository.create(createProceduresDto)
+
+        return this.procedureRepository.save(newProcedure)
+    }
+
+    /**
+     * Busca todos los procedimientos
+     * @returns Lista de procedimientos
+     */
+    async findAll(): Promise<Procedure[]> {
+        return this.procedureRepository.find()
+    }
+
+    /**
+     * Busca un procedimiento por su ID
+     * @param id - ID del procedimiento
+     * @returns El procedimiento encontrado
+     */
+    async findOne(id: string): Promise<Procedure> {
+        const procedure = await this.procedureRepository.findOne({ where: { id } })
+
+        if (!procedure) {
+            throw new NotFoundException(`Procedimiento ${id} no encontrado`)
+        }
+
+        return procedure;
+    }
+
+    /**
+     * Elimina un pago 
+     * @param id - ID del procedimiento
+     * @returns true si se elimina correctamente
+     */
+    async remove(id: string): Promise<boolean> {
+        const procedure = await this.findOne(id)
+        await this.procedureRepository.remove(procedure)
+        return true
+    }
 }
